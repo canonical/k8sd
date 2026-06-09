f260f388484a6f9d92e5a62b26cfd2014ff0f18c
Konstantinos Tsakalozos <kos.tsakalozos@canonical.com>
2026-06-09 17:27:05 +0300

    cleanup duplicate set default effort

--- pkg/k8sd/app/hooks_bootstrap.go
+++ pkg/k8sd/app/hooks_bootstrap.go
@@ -50,9 +50,6 @@ func (a *App) onBootstrap(ctx context.Context, s mctypes.State, initConfig map[s
 		return fmt.Errorf("failed to unmarshal bootstrap config: %w", err)
 	}
 
-	if bootstrapConfig.ClusterConfig.Network.Enabled != nil && *bootstrapConfig.ClusterConfig.Network.Enabled && bootstrapConfig.ClusterConfig.Network.KubeProxyEnabled == nil {
-		bootstrapConfig.ClusterConfig.Network.KubeProxyEnabled = utils.Pointer(false)
-	}
 	return a.onBootstrapControlPlane(ctx, s, bootstrapConfig)
 }
 
