0ee6334c9d678ff08c5224ce1c9c5b91aa26096e
Homayoon Alimohammadi <homayoon.alimohammadi@canonical.com>
2026-09-08 12:55:55 +0400

    fix: bump localpv to fix toleration issue (#82)
    Signed-off-by: Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>

--- pkg/k8sd/features/localpv/chart.go
+++ pkg/k8sd/features/localpv/chart.go
@@ -11,13 +11,13 @@ var (
 	Chart = helm.InstallableChart{
 		Name:         "ck-storage",
 		Namespace:    "kube-system",
-		ManifestPath: filepath.Join("charts", "rawfile-csi-0.9.2.tgz"),
+		ManifestPath: filepath.Join("charts", "rawfile-csi-0.9.3.tgz"),
 	}
 
 	// imageRepo is the repository to use for Rawfile LocalPV CSI.
 	imageRepo = "ghcr.io/canonical/rawfile-localpv"
 	// ImageTag is the image tag to use for Rawfile LocalPV CSI.
-	ImageTag = "0.8.3-ck4"
+	ImageTag = "0.8.3-ck5"
 
 	// csiNodeDriverImage is the image to use for the CSI node driver.
 	csiNodeDriverImage = "ghcr.io/canonical/csi-node-driver-registrar:2.17.0-ck0"
